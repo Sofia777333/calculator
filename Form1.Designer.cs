522545bc8fe150f4eb121ed437ff4542c401f1fd
Sofia <117852305+Sofia777333@users.noreply.github.com>
2022-11-10 12:25:45 +0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace калькулятор
+{
+    partial class Калькулятор
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlusMinusButton = new System.Windows.Forms.Button();
+            this.Button0 = new System.Windows.Forms.Button();
+            this.CommaButton = new System.Windows.Forms.Button();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button6 = new System.Windows.Forms.Button();
+            this.Button5 = new System.Windows.Forms.Button();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.Button9 = new System.Windows.Forms.Button();
+            this.Button8 = new System.Windows.Forms.Button();
+            this.Button7 = new System.Windows.Forms.Button();
+            this.RemoveLastChatbutton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.Footlabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Meterlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PlusMinusButton
+            // 
+            this.PlusMinusButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.PlusMinusButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.PlusMinusButton.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PlusMinusButton.Location = new System.Drawing.Point(417, 463);
+            this.PlusMinusButton.Name = "PlusMinusButton";
+            this.PlusMinusButton.Size = new System.Drawing.Size(95, 52);
+            this.PlusMinusButton.TabIndex = 0;
+            this.PlusMinusButton.Text = "±";
+            this.PlusMinusButton.UseVisualStyleBackColor = false;
+            this.PlusMinusButton.Click += new System.EventHandler(this.PlusMinusButton_Click);
+            // 
+            // Button0
+            // 
+            this.Button0.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Button0.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button0.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button0.Location = new System.Drawing.Point(518, 463);
+            this.Button0.Name = "Button0";
+            this.Button0.Size = new System.Drawing.Size(95, 52);
+            this.Button0.TabIndex = 1;
+            this.Button0.Text = "0";
+            this.Button0.UseVisualStyleBackColor = false;
+            this.Button0.Click += new System.EventHandler(this.Button0_Click);
+            // 
+            // CommaButton
+            // 
+            this.CommaButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.CommaButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.CommaButton.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CommaButton.Location = new System.Drawing.Point(619, 463);
+            this.CommaButton.Name = "CommaButton";
+            this.CommaButton.Size = new System.Drawing.Size(95, 52);
+            this.CommaButton.TabIndex = 2;
+            this.CommaButton.Text = ",";
+            this.CommaButton.UseVisualStyleBackColor = false;
+            this.CommaButton.Click += new System.EventHandler(this.CommaButton_Click);
+            // 
+            // Button3
+            // 
+            this.Button3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button3.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button3.Location = new System.Drawing.Point(619, 405);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(95, 52);
+            this.Button3.TabIndex = 5;
+            this.Button3.Text = "3";
+            this.Button3.UseVisualStyleBackColor = false;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Button2
+            // 
+            this.Button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button2.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button2.Location = new System.Drawing.Point(518, 405);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(95, 52);
+            this.Button2.TabIndex = 4;
+            this.Button2.Text = "2";
+            this.Button2.UseVisualStyleBackColor = false;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Button1
+            // 
+            this.Button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button1.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button1.Location = new System.Drawing.Point(417, 405);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(95, 52);
+            this.Button1.TabIndex = 3;
+            this.Button1.Text = "1";
+            this.Button1.UseVisualStyleBackColor = false;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Button6
+            // 
+            this.Button6.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button6.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button6.Location = new System.Drawing.Point(619, 347);
+            this.Button6.Name = "Button6";
+            this.Button6.Size = new System.Drawing.Size(95, 52);
+            this.Button6.TabIndex = 8;
+            this.Button6.Text = "6";
+            this.Button6.UseVisualStyleBackColor = false;
+            this.Button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // Button5
+            // 
+            this.Button5.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button5.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button5.Location = new System.Drawing.Point(518, 347);
+            this.Button5.Name = "Button5";
+            this.Button5.Size = new System.Drawing.Size(95, 52);
+            this.Button5.TabIndex = 7;
+            this.Button5.Text = "5";
+            this.Button5.UseVisualStyleBackColor = false;
+            this.Button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Button4
+            // 
+            this.Button4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button4.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button4.Location = new System.Drawing.Point(417, 347);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(95, 52);
+            this.Button4.TabIndex = 6;
+            this.Button4.Text = "4";
+            this.Button4.UseVisualStyleBackColor = false;
+            this.Button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Button9
+            // 
+            this.Button9.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button9.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button9.Location = new System.Drawing.Point(619, 289);
+            this.Button9.Name = "Button9";
+            this.Button9.Size = new System.Drawing.Size(95, 52);
+            this.Button9.TabIndex = 11;
+            this.Button9.Text = "9";
+            this.Button9.UseVisualStyleBackColor = false;
+            this.Button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // Button8
+            // 
+            this.Button8.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button8.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button8.Location = new System.Drawing.Point(518, 289);
+            this.Button8.Name = "Button8";
+            this.Button8.Size = new System.Drawing.Size(95, 52);
+            this.Button8.TabIndex = 10;
+            this.Button8.Text = "8";
+            this.Button8.UseVisualStyleBackColor = false;
+            this.Button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // Button7
+            // 
+            this.Button7.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button7.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button7.Location = new System.Drawing.Point(417, 289);
+            this.Button7.Name = "Button7";
+            this.Button7.Size = new System.Drawing.Size(95, 52);
+            this.Button7.TabIndex = 9;
+            this.Button7.Text = "7";
+            this.Button7.UseVisualStyleBackColor = false;
+            this.Button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // RemoveLastChatbutton
+            // 
+            this.RemoveLastChatbutton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.RemoveLastChatbutton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RemoveLastChatbutton.Font = new System.Drawing.Font("Wingdings", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.RemoveLastChatbutton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.RemoveLastChatbutton.Location = new System.Drawing.Point(619, 231);
+            this.RemoveLastChatbutton.Name = "RemoveLastChatbutton";
+            this.RemoveLastChatbutton.Size = new System.Drawing.Size(95, 52);
+            this.RemoveLastChatbutton.TabIndex = 13;
+            this.RemoveLastChatbutton.Text = " ";
+            this.RemoveLastChatbutton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.RemoveLastChatbutton.UseVisualStyleBackColor = false;
+            this.RemoveLastChatbutton.Click += new System.EventHandler(this.RemoveLastChatbutton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClearButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ClearButton.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ClearButton.Location = new System.Drawing.Point(518, 231);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(95, 52);
+            this.ClearButton.TabIndex = 12;
+            this.ClearButton.Text = "CE";
+            this.ClearButton.UseVisualStyleBackColor = false;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Footlabel
+            // 
+            this.Footlabel.AutoSize = true;
+            this.Footlabel.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Footlabel.Location = new System.Drawing.Point(41, 231);
+            this.Footlabel.Name = "Footlabel";
+            this.Footlabel.Size = new System.Drawing.Size(31, 34);
+            this.Footlabel.TabIndex = 14;
+            this.Footlabel.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(41, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 37);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Футы";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(41, 386);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 37);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Метры";
+            // 
+            // Meterlabel
+            // 
+            this.Meterlabel.AutoSize = true;
+            this.Meterlabel.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Meterlabel.Location = new System.Drawing.Point(41, 347);
+            this.Meterlabel.Name = "Meterlabel";
+            this.Meterlabel.Size = new System.Drawing.Size(31, 34);
+            this.Meterlabel.TabIndex = 18;
+            this.Meterlabel.Text = "0";
+            // 
+            // Калькулятор
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(734, 525);
+            this.Controls.Add(this.Meterlabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Footlabel);
+            this.Controls.Add(this.RemoveLastChatbutton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.Button9);
+            this.Controls.Add(this.Button8);
+            this.Controls.Add(this.Button7);
+            this.Controls.Add(this.Button6);
+            this.Controls.Add(this.Button5);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.CommaButton);
+            this.Controls.Add(this.Button0);
+            this.Controls.Add(this.PlusMinusButton);
+            this.Name = "Калькулятор";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlusMinusButton;
+        private System.Windows.Forms.Button Button0;
+        private System.Windows.Forms.Button CommaButton;
+        private System.Windows.Forms.Button Button3;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button6;
+        private System.Windows.Forms.Button Button5;
+        private System.Windows.Forms.Button Button4;
+        private System.Windows.Forms.Button Button9;
+        private System.Windows.Forms.Button Button8;
+        private System.Windows.Forms.Button Button7;
+        private System.Windows.Forms.Button RemoveLastChatbutton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Label Footlabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Meterlabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace калькулятор
+{
+
+    public partial class Калькулятор : Form
+    {
+        private const int MAX_VALUE_LENGTH = 10;
+
+        private string _valueToConvert = string.Empty;
+
+        private readonly char _comma = ',';
+
+
+        public Калькулятор()
+        {
+            InitializeComponent();
+        }
+        #region Buttons
+
+        private void Button1_Click(object sender, EventArgs e) => AssignNum(Button1);
+
+        private void Button2_Click(object sender, EventArgs e) => AssignNum(Button2);
+
+        private void Button3_Click(object sender, EventArgs e) => AssignNum(Button3);
+
+        private void Button4_Click(object sender, EventArgs e) => AssignNum(Button4);
+
+        private void Button5_Click(object sender, EventArgs e) => AssignNum(Button5);
+
+        private void Button6_Click(object sender, EventArgs e) => AssignNum(Button6);
+
+        private void Button7_Click(object sender, EventArgs e) => AssignNum(Button7);
+
+        private void Button8_Click(object sender, EventArgs e) => AssignNum(Button8);
+
+        private void Button9_Click(object sender, EventArgs e) => AssignNum(Button9);
+
+        private void Button0_Click(object sender, EventArgs e)
+        {
+            if (_valueToConvert.Length < 1)
+                return;
+            AssignNum(Button0);
+        }
+        #endregion
+
+        private void AssignNum(Button button)
+        {
+            if (_valueToConvert.Length >= MAX_VALUE_LENGTH)
+                return;
+
+            _valueToConvert += button.Text;
+
+            UpdateFootLabel();
+            UpdateMeterLabel();
+        }
+        private void UpdateFootLabel()
+        {
+            Footlabel.Text = _valueToConvert;
+        }
+        private void UpdateMeterLabel()
+        {
+            var valueToCovert = Convert.ToDouble(_valueToConvert);
+
+            var meters = valueToCovert * 0.3048;
+            string metText = meters.ToString();
+                  if (metText[metText.Length - 1] == _comma)
+                metText = metText.Remove(metText.Length - 1, 1);
+                 Meterlabel.Text = metText;
+             }
+
+        private void CommaButton_Click(object sender, EventArgs e)
+        {
+            if (_valueToConvert.Contains(_comma))
+                return;
+            AssignNum(CommaButton);
+        }
+
+        private void PlusMinusButton_Click(object sender, EventArgs e)
+        {
+            var value = Convert.ToDouble(_valueToConvert) * -1;
+            _valueToConvert = value.ToString();
+
+            UpdateFootLabel();
+            UpdateMeterLabel();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e) => AssignZeroValues();
+
+        private void AssignZeroValues()
+        {
+            _valueToConvert = string.Empty;
+            Footlabel.Text = "0";
+            Meterlabel.Text = "0";
+        }
+
+        private void RemoveLastChatbutton_Click(object sender, EventArgs e)
+        {
+           if (_valueToConvert.Length <= 1)
+            {
+                AssignZeroValues();
+                return;
+            }
+            _valueToConvert = _valueToConvert.Remove(_valueToConvert.Length - 1, 1);
+            UpdateFootLabel();
+            UpdateMeterLabel();
+        }
+    }
+}
+
+
+
+
